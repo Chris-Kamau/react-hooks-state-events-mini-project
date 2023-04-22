4c33041abcdb37c304b19070cf2d72624fd1a3d6
Chris <chris.kamau@student.moringaschool.com>
2023-04-22 09:43:53 +0300

    Completed Assignment

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList(props) {
+  const { tasks, selectedCategory } = props;
+  const [tasksState, setTasks] = useState(tasks);
+
+  const handleDelete = (id) => {
+    const updatedTasks = tasksState.filter((task) => task.id !== id);
+    setTasks(updatedTasks);
+  };
+
+  const filteredTasks = selectedCategory === "All" ? tasksState : tasksState.filter(task => task.category === selectedCategory);
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {filteredTasks.map((task) => (
+        <Task key={task.id} task={task} onDelete={() => handleDelete(task.id)} />
+      ))}
     </div>
   );
 }
